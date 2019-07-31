598645c0bce329001c720f4708136b576c9a5264
zhoulin2 <zhoulin2@asiainfo.com>
2019-07-31 17:55:50 +0800

    init

---
+++ user-svc/src/main/java/com/asiainfo/crm/order/OrderApplication.java
@@ -0,0 +1,14 @@
+package com.asiainfo.crm.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Steven
+ */
+@SpringBootApplication
+public class OrderApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+}
